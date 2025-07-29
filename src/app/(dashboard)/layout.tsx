c35a1f55b477f0add27b4c163c25ef67863bb1af
Canmi <9997200@qq.com>
2025-07-29 23:11:23 +0800

    add: smooth scroll

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-dvh" style={{ backgroundColor: 'var(--primary-color)' }}>
       <Sidebar />
-      <div className="flex-1 flex flex-col overflow-y-auto">
+      <div className="flex-1 flex flex-col overflow-y-auto scroll-smooth">
         <main className="min-h-dvh flex flex-col">
           {children}
         </main>
